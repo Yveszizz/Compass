8844a57c2febd7c01d862ced99618a3f7a7222d0
Yves NDJ <yndjamba@live.fr>
2020-10-07 14:16:21 +0200

    mapi test new map

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Header from './Header/Header';
 import Map from './Map/Map';
+import Mapi from './Map/Mapi';
 
 class App extends Component {
     
@@ -9,23 +10,10 @@ class App extends Component {
         return (
         <div>
             <Header />
-            <Map />
+            <Mapi />
         </div>
         )
     }
 }
 
-//export default App;
-
-/*export default GoogleApiWrapper({
-  apiKey: 'AIzaSyAfxtvx4AVwE5IuSH57EtkpW3CtGwMCtW4'
-})(App);*/
-
 export default App;
-
-/*<Map
-                google={this.props.google}
-                zoom={8}
-                //style={mapStyles}
-                initialCenter={{ lat:48.866667, lng:  2.333333}} />
-            <SearchPos />*/
--- src/Map/Map.js
+++ src/Map/Map.js
@@ -2,40 +2,45 @@ import React, {Component} from 'react';
 import './Map.css';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import SearchPos from './Search/SearchPos';
+import { GoogleMapsAPI } from '../client-config';
 
-const MapContainer = (props) => {
+class MapContainer extends Component {
+    state = {
+        markers: [],
+    }
     
-    const onMarkerClicke = (e) => {
+    onMarkerClicke = (e) => {
         console.log(e);
     }
     
-    const addMarker = () => {
-        new Marker ()
+    addMarker = (e) => {
+        console.log(e);
     }
     
-    return (
-        <Map google={props.google} zoom={14} className="map"
-        initialCenter={{ lat:48.866667, lng:  2.333333}}
-        onClick={addMarker}>
+    render() {
         
-        <Marker onClick={onMarkerClicke}
-                position={{lat:48.866667, lng:  2.333333}}
-                name='Paris' 
-                draggable={true}
-                
-                 />
-        <InfoWindow onClose={props.onInfoWindowClose}>
-            <div>
-              <h1>"ok"</h1>
-            </div>
-        </InfoWindow>
-      </Map>
-    )
+        const center = {
+            lat: 48.866667,
+            lng: 2.333333
+        }
+        
+        return (
+            <Map 
+                google={this.props.google}
+                zoom={8} 
+                className="map"
+                initialCenter={center}
+                onClick={this.addMarker} >
+          </Map>
+        )
+    }
 }
 
 export default GoogleApiWrapper({
-  apiKey: ''
+  apiKey: GoogleMapsAPI;
 })(MapContainer)
+
+
 
             /*<div>
               <h1>{this.state.selectedPlace.name}</h1>
